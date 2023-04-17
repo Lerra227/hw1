bd9d95851ac49a4c51b141c82f5e3a626c40a3d4
Lerra227 <amanatidilera@gmail.com>
2023-04-17 19:37:09 +0300

    fixed hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
-int main() {
-string name;
-std::cout << "Enter your name:" << std::endl;
-std::cin >> name;
-std::cout << "Hello world from " << name;
+int main()
+{
+ string name;
+ std::cout << "Enter your name:" << std::endl;
+ std::cin >> name;
+ std::cout << "Hello world from " << name;
 return 0;
 }
